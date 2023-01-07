8f61def4485235da6abe63debd87c6a7d7d2df74
pmoghle <110378952+pmoghle@users.noreply.github.com>
2023-01-07 17:32:15 +0530

    Update s3-bucket.tf
    updating private  bucket

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,14 @@
+# Using the data for current user
+data "aws_canonical_user_id" "current_user" {}
+
 # s3 private bucket
 resource "aws_s3_bucket" "private_s3_bucket" {
     bucket = "testbucket001" 
     acl = "private"   
+    
+    grant {
+      id          = data.aws_canonical_user_id.current_user.id
+      type        = "CanonicalUser"
+      permissions = ["FULL_CONTROL"]
+  }
 }

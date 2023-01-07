e3de4c67eb68214a08c85136661070d929a74c3c
pmoghle <110378952+pmoghle@users.noreply.github.com>
2023-01-07 17:44:02 +0530

    Update s3-bucket.tf
    added user permissions for full permissions

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -4,11 +4,29 @@ data "aws_canonical_user_id" "current_user" {}
 # s3 private bucket
 resource "aws_s3_bucket" "private_s3_bucket" {
     bucket = "testbucket001" 
-    acl = "private"   
+   # acl = "private"   
     
+   # grant {
+    #  id          = data.aws_canonical_user_id.current_user.id
+     # type        = "CanonicalUser"
+      #permissions = ["FULL_CONTROL"]
+  #}
+}
+
+
+resource "aws_s3_bucket_acl" "test-aws-s3-bucket-acl" {
+  bucket = aws_s3_bucket.private_s3_bucket.id
+  access_control_policy {
     grant {
-      id          = data.aws_canonical_user_id.current_user.id
-      type        = "CanonicalUser"
-      permissions = ["FULL_CONTROL"]
+      grantee {
+        id   = data.aws_canonical_user_id.current_user.id
+        type = "CanonicalUser"
+      }
+      permission = "FULL_CONTROL"
+    }
+
+    owner {
+      id = data.aws_canonical_user_id.current_user.id
+    }
   }
 }

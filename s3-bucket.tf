bf2c9faac02e0cd87c28b2fd87050307be9fefb6
pmoghle <110378952+pmoghle@users.noreply.github.com>
2023-01-08 22:48:33 +0530

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -4,7 +4,7 @@ data "aws_canonical_user_id" "current_user" {}
 # s3 private bucket
 resource "aws_s3_bucket" "private_s3_bucket" {
     bucket = "testbucket001" 
-   # acl = "private"   
+    acl = "private"   
     
    # grant {
     #  id          = data.aws_canonical_user_id.current_user.id
@@ -14,22 +14,22 @@ resource "aws_s3_bucket" "private_s3_bucket" {
 }
 
 
-resource "aws_s3_bucket_acl" "test-aws-s3-bucket-acl" {
-  bucket = aws_s3_bucket.private_s3_bucket.id
-  access_control_policy {
-    grant {
-      grantee {
-        id   = data.aws_canonical_user_id.current_user.id
-        type = "CanonicalUser"
-      }
-      permission = "FULL_CONTROL"
-    }
+# resource "aws_s3_bucket_acl" "test-aws-s3-bucket-acl" {
+#   bucket = aws_s3_bucket.private_s3_bucket.id
+#   access_control_policy {
+#     grant {
+#       grantee {
+#         id   = data.aws_canonical_user_id.current_user.id
+#         type = "CanonicalUser"
+#       }
+#       permission = "FULL_CONTROL"
+#     }
 
-    owner {
-      id = data.aws_canonical_user_id.current_user.id
-    }
-  }
-}
+#     owner {
+#       id = data.aws_canonical_user_id.current_user.id
+#     }
+#   }
+# }
 resource "aws_s3_bucket_object" "object" {
   bucket = "testbucket001"
   key    = "abc.txt"
@@ -39,6 +39,9 @@ resource "aws_s3_bucket_object" "object" {
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
   etag = filemd5("abc.txt")
+  depends_on=[
+	  aws_s3_bucket.private_s3_bucket
+  ]
 }
 
 #Resource to add bucket policy to a bucket 

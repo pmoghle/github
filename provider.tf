23b9d559609e8d648ec2b80903f030628e49a1e5
pmoghle <110378952+pmoghle@users.noreply.github.com>
2023-01-08 18:48:17 +0530

    Update provider.tf
    updating version to 4.47 for aws

--- provider.tf
+++ provider.tf
@@ -1,4 +1,15 @@
 
+#terraform block and version set
+terraform {
+  required_version = "~>1.1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>4.47.0"
+    }
+  }
+}
+
 #provider for AWS
 provider "aws" {
     profile="default"

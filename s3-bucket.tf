da9688daef6b70d67f521a9087cfe98e9e1c46ec
pmoghle <110378952+pmoghle@users.noreply.github.com>
2023-01-09 15:49:03 +0530

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -32,13 +32,13 @@ resource "aws_s3_bucket" "private_s3_bucket" {
 # }
 resource "aws_s3_bucket_object" "object" {
   bucket = "testbucket001"
-  key    = "abc.txt"
-  source = "abc.txt"
+  key    = "abc.html"
+  source = "abc.html"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
-  etag = filemd5("abc.txt")
+  etag = filemd5("abc.html")
   depends_on=[
 	  aws_s3_bucket.private_s3_bucket
   ]
